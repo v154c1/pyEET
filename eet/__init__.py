d90586de7f56175acabcbd034cadbac7e8b9d35e
Zdenek Travnicek <travnicek@iim.cz>
2016-10-15 23:22:30 +0000

    Fixed import in __init__.py

--- eet/__init__.py
+++ eet/__init__.py
@@ -1,4 +1,4 @@
-import EET
+from EET import EET
 from Trzba import *
 
 
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
 
-    e = EET.EET(sys.argv[1], sys.argv[2], 1, 'stodola')
+    e = EET(sys.argv[1], sys.argv[2], 1, 'stodola')
     p = e.create_payment('P12321', 3248, test=False)
     p.set_amount(TAX_BASIC, 1000, 210)
     r = e.send_payment(p)
